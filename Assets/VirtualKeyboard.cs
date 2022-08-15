e1df989651446c13f38ff8ac2437030840c0f60a
cemerdemdeniz <c-e-d@outlook.com.tr>
2022-08-15 15:18:00 +0100

    Backspace bug fix

--- Assets/VirtualKeyboard.cs
+++ Assets/VirtualKeyboard.cs
@@ -1,45 +1,64 @@
 ﻿using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class VirtualKeyboard : MonoBehaviour
 {
-
-
     public MyInputField InputField;
+    public VirtualKeyboard virtualKeyboard;
+
 
     bool m_isPressing;
+    bool m_isPressedLeft;
+    bool m_isPressedRight;
+    bool m_isKeyPressed;
+
 
     private void Update()
     {
+        if (m_isKeyPressed)
+        {
+            KeyPress("");
+        }
+
         if (m_isPressing)
         {
             KeyDelete();
         }
+        if (m_isPressedLeft)
+        {
+            KeyLeft();
+        }
+        if (m_isPressedRight)
+        {
+            KeyRight();
+        }
 
 
     }
+
+
 
     public void KeyPress(string c)
     {
-
         InputField.text += c;
         InputField.ActivateInputField();
-
-
+        InputField.UpdateCaretPosition();
     }
+
 
     public void KeyLeft()
     {
-
         string last = this.InputField.text;
-        for (int i = 0; i < last.Length; i++)
+        for (int i = 0 ; i < last.Length; i++)
         {
             if (i != last.Length)
             {
-                InputField.caretPosition = last.Length - 1;
+                InputField.caretPosition = InputField.selectionFocusPosition = i;
+                InputField.ActivateInputField();
             }
         }
     }
+
+
 
 
     public void KeyRight()
@@ -49,7 +68,8 @@ public class VirtualKeyboard : MonoBehaviour
         {
             if (i != last.Length)
             {
-                InputField.caretPosition = last.Length + 1;
+                InputField.caretPosition = InputField.selectionFocusPosition = i + 1;
+                InputField.ActivateInputField();
             }
         }
     }
@@ -71,22 +91,49 @@ public class VirtualKeyboard : MonoBehaviour
             }
         }
         this.InputField.text = newText;
-
+        InputField.ActivateInputField();
 
     }
 
 
-    public void IsPressingOn()
+    #region EventTriggerBoolean
+    public void IsBackSpaceOn()
     {
         m_isPressing = true;
     }
 
-
-    public void IsPressingOff()
+    public void IsBackSpaceOff()
     {
         m_isPressing = false;
-
     }
+
+    public void IsKeyLeftOn()
+    {
+        m_isPressedLeft = true;
+    }
+    public void IsKeyLeftOff()
+    {
+        m_isPressedLeft = false;
+    }
+    public void IsKeyRightOn()
+    {
+        m_isPressedRight = true;
+    }
+
+    public void IsKeyRightOff()
+    {
+        m_isPressedRight = false;
+    }
+
+    public void IsKeyPressed()
+    {
+        m_isKeyPressed = true;
+    }
+    public void IsKeyPressedOff()
+    {
+        m_isKeyPressed = false;
+    }
+    #endregion
 
 
 }
